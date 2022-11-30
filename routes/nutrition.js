116f294a7f7ad4d582aee410c3d92df52efbf946
Elpadraaa@gmail.com <Elpadraaa@gmail.com>
2022-11-30 14:30:34 +0300

    8.5

--- routes/nutrition.js
+++ routes/nutrition.js
@@ -1,23 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var Nutrition = require("../models/nutrition").nutrition
+var async = require("async");
+const { nutrition } = require('../models/nutrition');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с nutrition');
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с nutritions')
 });
 
-/* Страница героев */
+/* Страница котят */
 router.get('/:nick', function(req, res, next) {
-  Cat.findOne({nick:req.params.nick}, function(err,cat){
-      if(err) return next(err)
-      if(!cat) return next(new Error("Нет такого котенка в этом мультике"))
-      res.render('cat', {
-          title: cat.title,
-          picture: cat.avatar,
-          desc: cat.desc
+  async.parallel([
+          function(callback){
+              Nutrition.findOne({nick:req.params.nick}, callback)
+          },
+          function(callback){
+            Nutrition.find({},{_id:0,title:1,nick:1},callback)
+          }
+      ],
+      function(err,result){
+          if(err) return next(err)
+          var nutrition = result[0]
+          var nutritions = result[1] || []
+          if(!nutrition) return next(new Error("Нет такого котенка в мультике Три кота"))
+          res.render('nutrition', {
+              title: nutrition.title,
+              picture: nutrition.avatar,
+              desc: nutrition.desc,
+              menu: nutrition
+          });
       })
-  })
 })
 
-
-module.exports = router;
+module.exports = router
