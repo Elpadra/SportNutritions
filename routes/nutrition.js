a98e1f64f05569a6e58f9c50cdef1d87b0b84599
Elpadraaa@gmail.com <Elpadraaa@gmail.com>
2022-11-30 14:18:29 +0300

    8.4

--- routes/nutrition.js
+++ routes/nutrition.js
@@ -6,9 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с nutrition');
 });
 
-/* Страница добавок*/
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
+/* Страница героев */
+router.get('/:nick', function(req, res, next) {
+  Cat.findOne({nick:req.params.nick}, function(err,cat){
+      if(err) return next(err)
+      if(!cat) return next(new Error("Нет такого котенка в этом мультике"))
+      res.render('cat', {
+          title: cat.title,
+          picture: cat.avatar,
+          desc: cat.desc
+      })
+  })
+})
+
 
 module.exports = router;
