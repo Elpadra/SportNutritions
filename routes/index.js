0c08ef90709c445de0498d94329c2a2deb929625
Elpadra <Elpadraaa@gmail.com>
2022-12-24 19:11:58 +0300

    10.1

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var SportNutritions = require("../models/nutrition").SportNutritions
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', menu:menu, counter:req.session.counter });
+  SportNutritions.find({}, {_id: 0, title: 1, nick: 1}, function(err, menu) {
+    req.session.greeting = "Hi!!!"
+    res.render('index', {
+        title: "SportNutritions",
+        counter: req.session.counter
+  });
+})
 });
 
 
-/* Страница протеина */
-router.get('/protein', function(req, res, next) {
-    res.render('Nutritions', {
-        title: "Протеин",
-        picture: "images/protein.jpg",
-        desc: "Протеин (белок)– этоа спортивная добавка, что состоит из концентрата натуральных белков."
-    })
-});
-/* Страница бета-аланина */
-router.get('/beta-alanine', function(req, res, next) {
-    res.render('Nutritions', {
-        title: "Бета-аланин",
-        picture: "images/beta-alanine.jpg",
-        desc: "Бета-аланfин - биологически активная добавка к пище, дополнительный источник витамина D3, фолиевой кислоты, изофлавонов, содержащая бета-аланин."
-    })
-});
-/* Страница аминокислоты */
-router.get('/bcaa', function(req, res, next) {
-    res.render('Nutritions', {
-        title: "Аминокислота",
-        picture: "images/bcaa.jpg",
-        desc: "BCAA – основной материал для построения новых мышц. Эти аминокислоты составляют 35% всей мышечной массы и принимают важное участие в процессах анаболизма и восстановления, обладают антикатаболическим действием."
-    })
-});
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход'});
+  });
+  
+
+
 module.exports = router;
