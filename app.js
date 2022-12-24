0cb41de2c3d2db506b5b3e84a9229b354a73ac8b
Elpadra <Elpadraaa@gmail.com>
2022-12-24 19:05:18 +0300

    9.5

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/SportNutritions')
 var nutrition = require('./routes/nutrition');
 var session = require("express-session")
-
+var SportNutritions = require("./models/nutrition").SportNutritions
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -33,10 +33,22 @@ app.use(session({
     saveUninitialized: true,
     store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1:27017/SportNutritions'})	
 }))
+
 app.use(function(req,res,next){
     req.session.counter = req.session.counter +1 || 1
     next()
 })
+
+app.use(function(req,res,next){
+    res.locals.nav = []
+    SportNutritions.find(null,{_id:0,title:1,nick:1},function(err,result){
+        if(err) throw err
+        res.locals.nav = result
+        next()
+    })
+})
+
+app.use(require("./middleware/createMenu.js"))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ middleware/createMenu.js
@@ -0,0 +1,13 @@
+var SportNutritions = require("./../models/nutrition").SportNutritions
+
+
+module.exports = function(req,res,next){
+    res.locals.nav = []
+
+    SportNutritions.find(null,{_id:0,title:1,nick:1},function(err,result){
+        if(err) throw err
+        res.locals.nav = result
+        next()
+    })
+
+}
